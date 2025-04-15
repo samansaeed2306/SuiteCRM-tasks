297e16d5cad1c57ee62242a3f38615223926eec4
Saman Saeed <samansaeed2306@gmail.com>
2025-04-15 17:44:37 +0500

    Require Accountname with full definition

--- custom/Extension/modules/Leads/Ext/Vardefs/accountname_required.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/accountname_required.php
@@ -1,2 +1,22 @@
 <?php
-$dictionary['Lead']['fields']['account_name']['required'] = true;
+$dictionary['Lead']['fields']['account_name'] = array(
+    'name' => 'account_name',
+    'vname' => 'LBL_ACCOUNT_NAME',
+    'type' => 'varchar',
+    'len' => 255,
+    'required' => true,
+    'audited' => true,
+    'massupdate' => true,
+    'duplicate_merge' => 'enabled',
+    'merge_filter' => 'enabled',
+    'calculated' => false,
+    'size' => '20',
+    'enable_range_search' => false,
+    'importable' => 'required',
+    'duplicate_merge_dom_value' => '1',
+    'studio' => 'visible',
+    'reportable' => true,
+    'default' => '',
+   
+);
+
--- custom/modules/Leads/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/Leads/Ext/Vardefs/vardefs.ext.php
@@ -2,7 +2,27 @@
  //WARNING: The contents of this file are auto-generated
 
 
-$dictionary['Lead']['fields']['account_name']['required'] = true;
+$dictionary['Lead']['fields']['account_name'] = array(
+    'name' => 'account_name',
+    'vname' => 'LBL_ACCOUNT_NAME',
+    'type' => 'varchar',
+    'len' => 255,
+    'required' => true,
+    'audited' => true,
+    'massupdate' => true,
+    'duplicate_merge' => 'enabled',
+    'merge_filter' => 'enabled',
+    'calculated' => false,
+    'size' => '20',
+    'enable_range_search' => false,
+    'importable' => 'required',
+    'duplicate_merge_dom_value' => '1',
+    'studio' => 'visible',
+    'reportable' => true,
+    'default' => '',
+   
+);
+
 
 
 
