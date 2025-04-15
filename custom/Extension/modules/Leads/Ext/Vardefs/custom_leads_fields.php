d9a3e2bc1c505c96996fe60a1ef2c0ba40f4b262
Saman Saeed <samansaeed2306@gmail.com>
2025-04-15 17:34:12 +0500

    Add custom fields to Leads_cstm table

--- custom/Extension/modules/Leads/Ext/Vardefs/custom_leads_fields.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/custom_leads_fields.php
@@ -1,17 +1,27 @@
 <?php
 
-
 $dictionary['Lead']['fields']['existing_c'] = array(
     'name' => 'existing_c',
     'vname' => 'LBL_EXISTING',
     'type' => 'bool',
     'default' => 0,
+    'source' => 'custom_fields',  // Ensures it is placed in the cstm table
+    'required' => false,
+    'audited' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'importable' => true,
 );
-
 
 $dictionary['Lead']['fields']['user_remarks_c'] = array(
     'name' => 'user_remarks_c',
     'vname' => 'LBL_USER_REMARKS',
     'type' => 'varchar',
     'len' => 255,
+    'source' => 'custom_fields',  // Ensures it is placed in the cstm table
+    'required' => false,
+    'audited' => false,
+    'reportable' => true,
+    'unified_search' => false,
+    'importable' => true,
 );
