7e2597ffb0431c12fa6000354007a4cf73c84628
Saman Saeed <samansaeed2306@gmail.com>
2025-04-16 12:11:53 +0500

    Update dropdown list items

---
+++ custom/include/language/en_us.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$app_list_strings['lead_source_dom'] = array(
+    '' => '',
+    'Web Site' => 'Web Site',
+    'Email' => 'Email',
+    'Campaign' => 'Campaign',
+    'Other' => 'Other',
+);
